74e56b4d01b91147b6af56022031148177f257b9
CalokTI <72436270+CalokTI@users.noreply.github.com>
2021-02-04 15:12:44 +0100

    Kvadrat class

--- src/Kvadrat.java
+++ src/Kvadrat.java
@@ -1,3 +1,14 @@
 public class Kvadrat {
-
+    
+    public void printKvadrat(int nums){
+        for (int i = 0; i < nums; i++) {
+            for (int j = 0; j < nums; j++) {
+                System.out.print(" * ");
+            }
+            if(i != nums-1) {
+                System.out.println();
+            }
+        }
+    }
+            
 }
