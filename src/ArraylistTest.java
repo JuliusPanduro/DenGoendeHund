ee3268f0f03d0619b9028f1538b71d1fd35e6637
JuliusPanduro <73829015+JuliusPanduro@users.noreply.github.com>
2021-02-04 15:49:58 +0100

    Arraylist done

--- src/ArraylistTest.java
+++ src/ArraylistTest.java
@@ -4,6 +4,7 @@ public class ArraylistTest {
 
     public boolean doesArraylistContainString(ArrayList<String> list, String text) {
         if (list.contains(text)) {
+            System.out.println("String has been found");
             return true;
         } else {
             list.add(text);
