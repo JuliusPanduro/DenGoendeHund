57eec3b55a3cc5fd8bc1ed00361b75dd96370965
Anton <anto8214@stud.kea.dk>
2021-02-04 15:16:17 +0100

    Fjernede gamle klasser, og tilføjede 'grass'
    (cherry picked from commit 128366834e0c097b4218f2db8b83d31220e9e066)
    
    # Conflicts:
    #	src/Grass.java

--- src/Grass.java
+++ src/Grass.java
@@ -1,16 +1,2 @@
 public class Grass {
-// Anton
-
-    private final double grassGrowth = 0.8;
-
-    public void CutGrassDeadline (double currentLength, double maxLength){
-
-        double deadline = (maxLength-currentLength)/grassGrowth;
-
-        System.out.println("You must cut the grass within " + deadline + " days, or it will succeed your max length, " + maxLength + " cm");
-
-    }
-
-
-
 }
