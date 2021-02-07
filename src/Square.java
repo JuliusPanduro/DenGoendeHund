6ddbdac65836f4634340bea031d39490eac44b0a
CalokTI <72436270+CalokTI@users.noreply.github.com>
2021-02-07 12:31:23 +0100

    ProperCase.java added and completed. Square.java updated with extra task. StringSearch.java updated with extra task.

--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,11 @@
+/**
+ * Skriv en metode, der som parameter modtager et heltal. Metoden skal udskrive et kvadrat på skærmen svarende til heltallet.
+ * • Udvid metoden, så den også modtager et tegn, som er det tegn, som printes på skærmen.
+ *
+ * @author Carsten
+ * Time: -
+ * Date: -
+ */
 public class Square {
     
     public void printSquare(int nums){
@@ -10,5 +18,17 @@ public class Square {
             }
         }
     }
+
+    public void printSquareWithChar(int nums, char ch){
+        for (int i = 0; i < nums; i++) {
+            for (int j = 0; j < nums; j++) {
+                System.out.print(" " + ch + " ");
+            }
+            if(i != nums-1) {
+                System.out.println();
+            }
+        }
+    }
+
             
 }
--- src/StringSearch.java
+++ src/StringSearch.java
@@ -1,9 +1,28 @@
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
+/**
+ * Skriv en metode, der som parametre modtager et array af typen String samt en String.
+ * Metoden skal undersøge om strengen findes i arrayet.
+ * Metoden skal returnere indekspositionen, hvis strengen findes i arrayet og –1 hvis strengen ikke findes i arrayet.
+ * • Lav en alternativ fejlhåndtering, hvis strengen ikke findes i arrayet, hvor du i stedet kaster en exception.
+ *
+ * @author Carsten
+ * Time: -
+ * Date: -
+ */
 public class StringSearch {
 
     public int stringSearchInStringArray(String[] list, String str){
 
+        return Arrays.asList(list).indexOf(str);
+    }
+
+    public int stringSearchInStringArrayWithException(String[] list, String str){
+
+        if(!Arrays.asList(list).contains(str)){
+            throw new NoSuchElementException();
+        }
         return Arrays.asList(list).indexOf(str);
     }
 }
